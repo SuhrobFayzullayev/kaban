d15efacd46809f81670286f7d00068198e7ec518
Suhrob Fayzullayev <umar@Suhrobs-MacBook-Air.local>
2025-02-03 12:36:37 +0500

    last changes

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+})
+</script>
+
 <template>
-  <div class="flex h-full min-h-screen w-full items-center justify-center bg-clight p-6">index page</div>
+  <div class="flex h-full min-h-screen w-full items-center justify-center bg-clight p-6">
+    <div>
+      <h1>KABAN DASHBOARD</h1>
+      <div class="mx-auto flex w-fit justify-between gap-[20px]">
+        <NuxtLink to="/login" class="underline">Login</NuxtLink>
+        <NuxtLink to="/register" class="underline">Sign up</NuxtLink>
+      </div>
+    </div>
+  </div>
 </template>
